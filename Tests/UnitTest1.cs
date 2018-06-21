7543c94ef4c7ecb03957acbcf9c40cba62851acd
Derrick Shields <dshields@pivotal.io>
2018-06-21 18:44:25 -0400

    Added unit tests

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using RazorPagesMovie;
+using Xunit;
+
+namespace Tests
+{
+    public class UnitTest1 : IClassFixture<WebApplicationTestFixture<Startup>>
+    {
+        public HttpClient Client { get; }
+
+        public UnitTest1(WebApplicationTestFixture<Startup> fixture)
+        {
+            Client = fixture.Client;
+        }
+
+        [Fact]
+        public async Task GetHomePage()
+        {
+            var response = await Client.GetAsync("/");
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
