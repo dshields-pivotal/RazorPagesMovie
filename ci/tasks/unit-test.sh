edf0ec6a85f2a14a5de040f8f19cf3980ae0723e
Derrick Shields <dshields@pivotal.io>
2018-06-25 13:07:02 -0400

    Fixed test pipeline

--- ci/tasks/unit-test.sh
+++ ci/tasks/unit-test.sh
@@ -10,6 +10,6 @@ fi
 TASK_ROOT=$(pwd)
 [[ -d "${TASK_ROOT}/nuget" && ! -d "${HOME}/.nuget" ]] && ln -s "${TASK_ROOT}/nuget" "${HOME}/.nuget"
 
-cd ${TASK_ROOT}/service-checkout
+cd ${TASK_ROOT}/service-checkout/Tests
 dotnet restore
 dotnet test
